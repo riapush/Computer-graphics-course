5fa123de094a57b86a7d407e9aaf664269b6c913
riapush <leriya022@gmail.com>
2024-03-03 21:52:06 +0300

    fixed error
    с новой картинкой сильно лучше не стало, так что оставила старую

--- Computer_graphics/Computer_graphics/CubeMapPixelShader.hlsl
+++ Computer_graphics/Computer_graphics/CubeMapPixelShader.hlsl
@@ -1,9 +1,9 @@
-TextureCube colorTexture : register (t0);
-SamplerState colorSampler : register (s0);
+Texture2D colorTexture : register(t0);
+SamplerState colorSampler : register(s0);
 
 struct PS_INPUT {
-  float4 position : SV_POSITION;
-  float3 localPos : POSITION1;
+    float4 position : SV_POSITION;
+    float3 localPos : POSITION1;
 };
 
 float4 main(PS_INPUT input) : SV_TARGET{
